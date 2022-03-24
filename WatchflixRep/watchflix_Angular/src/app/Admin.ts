ceb5cfbb335d541301698f832e2b97e889254db9
John Atillo <80560912+codyatillo@users.noreply.github.com>
2022-03-24 17:56:35 -0500

    Delete WatchflixRep directory

--- WatchflixRep/watchflix_Angular/src/app/Admin.ts
+++
@@ -1,9 +0,0 @@
-export class Admin{
-    constructor(
-        public adminId: string,
-        public adminName: string,
-        public adminUsername: string,
-        public adminPassword: string,
-        public adminEmail: string,
-    ){}
-}
--- WatchflixRep/watchflix_Angular/src/app/changeUser.ts
+++
@@ -1,9 +0,0 @@
-export class changeUser{
-    constructor(
-        public username: string,
-        public blocked: boolean, 
-        public subscribe: boolean,
-        public activity: boolean
-        
-    ){}
-}
